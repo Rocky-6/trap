896c15a56beb1e85c684c3f7c88856664396c788
Rocky-6 <imu387058@gmail.com>
2023-03-19 01:38:13 +0900

    kick

--- instruments/kick.go
+++ instruments/kick.go
@@ -15,25 +15,25 @@ func mkKick() {
 
 	// start
 	// 1
-	tr.Add(0, midi.NoteOn(0, midi.C(3), 100))
-	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
-	tr.Add(clock.Ticks64th()*23, midi.NoteOn(0, midi.C(3), 100))
-	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
-	tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.C(3), 100))
-	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
-	tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.C(3), 100))
-	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
+	tr.Add(0, midi.NoteOn(0, midi.Db(3), 100))
+	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
+	tr.Add(clock.Ticks64th()*23, midi.NoteOn(0, midi.Db(3), 100))
+	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
+	tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.Db(3), 100))
+	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
+	tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.Db(3), 100))
+	tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
 
 	// loop
 	for loop := 0; loop < 4; loop++ {
-		tr.Add(clock.Ticks64th()*7, midi.NoteOn(0, midi.C(3), 100))
-		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
-		tr.Add(clock.Ticks64th()*23, midi.NoteOn(0, midi.C(3), 100))
-		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
-		tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.C(3), 100))
-		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
-		tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.C(3), 100))
-		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.C(3)))
+		tr.Add(clock.Ticks64th()*7, midi.NoteOn(0, midi.Db(3), 100))
+		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
+		tr.Add(clock.Ticks64th()*23, midi.NoteOn(0, midi.Db(3), 100))
+		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
+		tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.Db(3), 100))
+		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
+		tr.Add(clock.Ticks64th()*15, midi.NoteOn(0, midi.Db(3), 100))
+		tr.Add(clock.Ticks64th(), midi.NoteOff(0, midi.Db(3)))
 	}
 
 	// end
