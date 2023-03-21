fd61f49a339ff98d721153fee5ea4de8f0ad4881
Rocky-6 <imu387058@gmail.com>
2023-03-21 14:13:27 +0900

    unittest

---
+++ instruments/unit_test.go
@@ -0,0 +1,13 @@
+package instruments
+
+import (
+	"testing"
+
+	"github.com/Rocky-6/trap/database"
+)
+
+func TestUnit(t *testing.T) {
+	cp := database.DispChord()
+	MkBass("..", "C", cp)
+	MkChord("..", "C", cp)
+}
