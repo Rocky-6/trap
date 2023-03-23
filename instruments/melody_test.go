79994a0c34e19c5be9ab14f5deeda9ff7a459dba
Rocky-6 <imu387058@gmail.com>
2023-03-24 08:13:06 +0900

    melody

---
+++ instruments/melody_test.go
@@ -0,0 +1,7 @@
+package instruments
+
+import "testing"
+
+func TestMkMelody(t *testing.T) {
+	MkMelody("..", "C")
+}
--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"flag"
 
+	"github.com/Rocky-6/trap/database"
 	"github.com/Rocky-6/trap/instruments"
 )
 
 func main() {
 	flag.Parse()
 	path := flag.Arg(0)
+	key := flag.Arg(1)
 	instruments.MkKick(path)
 	instruments.MkClap(path)
 	instruments.MkHihat(path)
+	cp := database.DispChord()
+	instruments.MkBass(path, key, cp)
+	instruments.MkChord(path, key, cp)
 }
